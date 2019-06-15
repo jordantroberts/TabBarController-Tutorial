cffe40c5b57ff32a026637dfa2b46186d5fdc4ce
Jordan Roberts <jordanroberts@talk21.com>
2019-06-15 11:46:12 +0100

    Created ProfileViewController and set up outlet for Logout

---
+++ TabControllerSwiftTutorial/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  TabControllerSwiftTutorial
+//
+//  Created by Jordan Roberts on 15/06/2019.
+//  Copyright © 2019 Jordan Roberts. All rights reserved.
+//
+
+import Foundation
+import UIKit // contains the ViewController information
+
+class ProfileViewController: UIViewController {
+    
+    var userName: String? 
+    var passWord: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func Logout(_ sender: Any) {
+    }
+}
